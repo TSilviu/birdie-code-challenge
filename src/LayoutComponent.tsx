750854f50dabc7a22dbf61f7df81570cefab5fc0
TSilviu <t.silviu94@gmail.com>
2017-08-13 20:39:44 +0100

    seleted value is passed up to the layout component - can be used to query results

--- src/LayoutComponent.tsx
+++ src/LayoutComponent.tsx
@@ -4,28 +4,34 @@ import { SelectorComponent } from './SelectorComponent';
 
 interface IState {
   columns: string[];
+  values: any[];
 }
 
 export class LayoutComponent extends React.Component<{}, IState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      columns: []
+      columns: [],
+      values: []
     }
+  }
+
+  private getDataForValue(value: string): void {
+    fetch('/values')
+      .then( (response) => response.json() )
+      .then( (values) => this.setState({ values }));
   }
 
   public componentDidMount() {
     fetch('/columns')
       .then( (response) => response.json() )
-      .then( (columns) => this.setState({
-        columns,
-      }));
+      .then( (columns) => this.setState({ columns }));
   }
 
   public render(): JSX.Element {
     return (
       <div>
-        <SelectorComponent columns={this.state.columns} />
+        <SelectorComponent columns={this.state.columns} getDataForValue={this.getDataForValue.bind(this)}/>
       </div>
     );
   }
--- src/SelectorComponent.tsx
+++ src/SelectorComponent.tsx
@@ -2,13 +2,24 @@ import * as React from "react";
 
 interface IProps {
   columns: string[];
+  getDataForValue: (option: string) => {};
 }
 
 export class SelectorComponent extends React.Component<IProps, {}> {
+  private onChange(e: React.FormEvent<HTMLSelectElement>) {
+    const selectedValue = e.currentTarget.value;
+    this.props.getDataForValue(selectedValue);
+  }
+
+  private createListOptions(value: string): JSX.Element {
+    return <option key={value} value={value}> { value } </option>
+  }
+
   public render(): JSX.Element {
+    const content = this.props.columns.map( (e) => this.createListOptions(e) );
     return (
       <div>
-        This is where the selector is going to be
+        Select variable: <select id="selectBox" onChange={this.onChange.bind(this)}> { content } </select>
       </div>
     );
   }
